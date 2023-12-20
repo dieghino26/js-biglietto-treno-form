6e88ce4607f395cf0fcf2d6cca7caa2fc31cda10
dieghino26 <brotzadiego@gmail.com>
2023-12-20 17:51:00 +0100

    recovered elements

---
+++ js/script.js
@@ -0,0 +1,19 @@
+//recuperare elementi 
+const nameField = document.getElementById("name")
+const kmsField = document.getElementById("kms")
+const ageField = document.getElementById("age")
+
+
+const confirmButton = document.getElementById("confirm")
+
+const ticketElements = document.getElementById("ticket-elements")
+const passengerName = document.getElementById("passenger-name")
+const passengerRate = document.getElementById("rate")
+const passengerCar = document.getElementById("car")
+const passengerCp = document.getElementById("cp")
+const passengerPrice = document.getElementById("price")
+
+//variabili
+let pricePerKms = 0.21
+let rateName = "Tariffa Ordinaria"
+
